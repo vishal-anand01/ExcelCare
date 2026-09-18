f96a0ce16749a493709e3176dc1cb14b8d8f0e6e
Vishal Kumar Anand <vishal.anand6219@gmail.com>
2026-09-18 14:28:43 +0530

    Fix GitHub Pages routing

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,6 +1,11 @@
-
 import { useEffect } from "react";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+
 import { AboutUsPage } from "../pages/AboutUsPage";
 import { BlogPage } from "../pages/BlogPage";
 import { BlogArticlePage } from "../pages/Blog/BlogArticlePage";
@@ -22,7 +27,11 @@ function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
   }, [pathname]);
 
   return null;
@@ -30,24 +39,54 @@ function ScrollToTop() {
 
 export function AppRouter() {
   return (
-    <BrowserRouter basename="/ExcelCare">
+    <BrowserRouter basename="/ExcelCare/">
       <ScrollToTop />
+
       <Routes>
+        {/* Home */}
         <Route path="/" element={<HomePage />} />
+
+        {/* Main Pages */}
         <Route path="/about-us" element={<AboutUsPage />} />
         <Route path="/services" element={<ServicesPage />} />
-        <Route path="/laundry-services" element={<LaundryServicesPage />} />
+        <Route
+          path="/laundry-services"
+          element={<LaundryServicesPage />}
+        />
         <Route path="/dry-cleaning" element={<DryCleaningPage />} />
         <Route path="/ironing" element={<IroningPage />} />
         <Route path="/shoe-cleaning" element={<ShoeCleaningPage />} />
-        <Route path="/curtain-cleaning" element={<CurtainCleaningPage />} />
-        <Route path="/carpet-cleaning" element={<CarpetCleaningPage />} />
+        <Route
+          path="/curtain-cleaning"
+          element={<CurtainCleaningPage />}
+        />
+        <Route
+          path="/carpet-cleaning"
+          element={<CarpetCleaningPage />}
+        />
+
+        {/* Contact & FAQ */}
         <Route path="/contact-us" element={<ContactUsPage />} />
         <Route path="/faqs" element={<FaqPage />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-        <Route path="/terms-and-conditions" element={<TermsConditionsPage />} />
+
+        {/* Legal Pages */}
+        <Route
+          path="/privacy-policy"
+          element={<PrivacyPolicyPage />}
+        />
+        <Route
+          path="/terms-and-conditions"
+          element={<TermsConditionsPage />}
+        />
+
+        {/* Blog */}
         <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:slug" element={<BlogArticlePage />} />
+        <Route
+          path="/blog/:slug"
+          element={<BlogArticlePage />}
+        />
+
+        {/* 404 */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
